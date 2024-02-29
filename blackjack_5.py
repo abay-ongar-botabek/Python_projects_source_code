f751bd983d18dee5b71246a956f49dcf05a27796
abay.ongar <abay.ongar.botabek@gmail.com>
2024-02-29 21:55:41 +0600

    jasyryn karta jane tuz mani

--- blackjack_5.py
+++ blackjack_5.py
@@ -5,7 +5,7 @@ QIYQ = chr(9830)
 QARGA = chr(9824)
 SHYBYN = chr(9827)
 
-KARTA_ARTY = 'backside'
+JASYRYN_KARTA = 'jabyq_karta'
 
 def main():
     aqsha = 5000
@@ -14,7 +14,7 @@ def main():
     dilerQoly = [koloda.pop(), koloda.pop()]
     oiynshyQoly = [koloda.pop(), koloda.pop()]
     
-    qoldyKorsetu(dilerQoly, oiynshyQoly, True)
+    qoldyKorsetu(dilerQoly, oiynshyQoly, False)
 
 def upaidySanau(qoldagyKarta):
     upai = 0
@@ -26,17 +26,17 @@ def upaidySanau(qoldagyKarta):
             upai += 10
 
     for rank, mast in qoldagyKarta:
-        if rank == 'Т' and upai < 10:
+        if rank == 'Т' and upai <= 10:
             upai += 11
         elif rank == 'Т' and upai > 10:
             upai += 1
     return upai
     
-def qoldyKorsetu(dilerQoly, oiynshyQoly, dilerQolynKorsetu):
+def qoldyKorsetu(dilerQoly, oiynshyQoly, dilerQolynKorsetpeu):
     dilerUpaiy = upaidySanau(dilerQoly)
-    if KARTA_ARTY:
+    if dilerQolynKorsetpeu:
         print('Дилер қолы: ???')
-        kartaSuretinSalu([KARTA_ARTY] + dilerQoly[1:])
+        kartaSuretinSalu([JASYRYN_KARTA] + dilerQoly[1:])
     else:
         print(f'Дилер қолы: {dilerUpaiy} ұпай')
         kartaSuretinSalu(dilerQoly)
@@ -48,7 +48,7 @@ def kartaSuretinSalu(kartalar):
     qatarlar = ['', '', '', '', '']
     for i, karta in enumerate(kartalar):
         qatarlar[0] += ' ___ '
-        if karta == KARTA_ARTY:
+        if karta == JASYRYN_KARTA:
             qatarlar[1] += '|#  |'
             qatarlar[2] += '| ##|'
             qatarlar[3] += '|__#|'
