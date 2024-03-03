06c6aafc191b984f4dcf40ce62e03ca3d379babe
abay.ongar <abay.ongar.botabek@gmail.com>
2024-03-03 12:09:57 +0500

    oiyndy zhalgastyramyz ba 2

--- bagels_2.py
+++ bagels_2.py
@@ -4,39 +4,44 @@ MAX_SANDAR = 3
 MUMKINDIKTER_SANY = 10
 
 def main():
-    MUMKINDIKTER_SANY = 1
-    jasyrynSan = jasyrynSandyAlu()
-    qoldanushyEngizgenSan = ''
     while True:
-        qoldanushyEngizgenSan = input('3 цифрдан тұратын санды енгізіңіз: > ')
-        if jasyrynSan == qoldanushyEngizgenSan:
-            print('Сіз жасырын санды таптыңыз.')
-        elif len(qoldanushyEngizgenSan) < 3 or len(qoldanushyEngizgenSan) >= 4:
-            continue
-        else:
-            print('Тағы байқап көріңіз')
-            print(MUMKINDIKTER_SANY)
-            MUMKINDIKTER_SANY += 1
-            if MUMKINDIKTER_SANY > 10:
-                print('Өкінішке орай сіз жасырын санды таппадыңыз.')
-                print(f'Жасырын сан: {jasyrynSan} болатын')
-                jasyrynSan = jasyrynSandyAlu()
-                MUMKINDIKTER_SANY = 1
+        print('Мен 3 цифрлы сан ойладым. Тауып көр')
+        oiynshyZhasaganMumkindikSany = 0
+
+        while oiynshyZhasaganMumkindikSany < MUMKINDIKTER_SANY:
+            print(f'Болжам #{oiynshyZhasaganMumkindikSany + 1}')
+            oiynshyBoljaganSan = input('> ')
+            print(oiynshyBoljaganSan)
+            jasyrynSan = jasyrynSandyAlu()
+            if oiynshyBoljaganSan == jasyrynSan:
+                print('Жарайсыз. Сіз жасырын санды таптыңыз!')
+                break
+
+            oiynshyZhasaganMumkindikSany += 1
+
+            if oiynshyZhasaganMumkindikSany == MUMKINDIKTER_SANY:
+                print('Сіз таба алмадыңыз!')
+                print(f'Жасырын сан {jasyrynSan} болатын')
+                break
+
+        oiyndyJalgastyramyzba = input('Ойынды жалғасырамыз ба? (иә / жоқ) > ')
+        if not oiyndyJalgastyramyzba.lower().startswith('и'):
+            break
+        
 
 def tuspaldapKomekBeru(qoldanushyEngizgenSan, jasyrynSan):
     if qoldanushyEngizgenSan == jasyrynSan:
         print('Жарайсыз. Сіз жасырын санды таптыңыз!')
     
     komekHabarlama = []
-    for i in (len(qoldanushyEngizgenSan)):
+    for i in range(len(qoldanushyEngizgenSan)):
         if qoldanushyEngizgenSan[i] == jasyrynSan[i]:
             komekHabarlama.append('Fermi')
         elif qoldanushyEngizgenSan[i] in jasyrynSan:
             komekHabarlama.append('Pico')
         elif qoldanushyEngizgenSan[i] not in jasyrynSan:
             komekHabarlama.append('Bagels')
-    ' '.join(komekHabarlama)
-    return komekHabarlama
+    return ' '.join(komekHabarlama)
 
 def jasyrynSandyAlu():
     jalpySandarTizimi = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
