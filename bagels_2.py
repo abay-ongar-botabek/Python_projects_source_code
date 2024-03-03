26caf5e8284684de4dfc6a9d496d60082ecdea7b
abay.ongar <abay.ongar.botabek@gmail.com>
2024-03-03 09:53:53 +0500

    mumkindikter sany

--- bagels_2.py
+++ bagels_2.py
@@ -1,17 +1,25 @@
 import random
 
 MAX_SANDAR = 3
+MUMKINDIKTER_SANY = 0
 
 def main():
+    MUMKINDIKTER_SANY = 1
     jasyrynSan = jasyrynSandyAlu()
     qoldanushyEngizgenSan = ''
     while True:
-        qoldanushyEngizgenSan = input('Санды енгізіңіз: >')
+        qoldanushyEngizgenSan = input('Санды енгізіңіз: > ')
         if jasyrynSan == qoldanushyEngizgenSan:
             print('Сіз жасырын санды таптыңыз.')
         else:
             print('Тағы байқап көріңіз')
-    
+            print(MUMKINDIKTER_SANY)
+            MUMKINDIKTER_SANY += 1
+            if MUMKINDIKTER_SANY > 10:
+                print('Өкінішке орай сіз жасырын санды таппадыңыз.')
+                print(f'Жасырын сан: {jasyrynSan} болатын')
+                jasyrynSan = jasyrynSandyAlu()
+                MUMKINDIKTER_SANY = 1
 
 def jasyrynSandyAlu():
     jalpySandarTizimi = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
