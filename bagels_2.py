c61b6ab0be77d7f834d8b40be193f4e9143581d0
abay.ongar <abay.ongar.botabek@gmail.com>
2024-03-03 10:50:51 +0500

    komek habarlama

--- bagels_2.py
+++ bagels_2.py
@@ -1,16 +1,18 @@
 import random
 
 MAX_SANDAR = 3
-MUMKINDIKTER_SANY = 0
+MUMKINDIKTER_SANY = 10
 
 def main():
     MUMKINDIKTER_SANY = 1
     jasyrynSan = jasyrynSandyAlu()
     qoldanushyEngizgenSan = ''
     while True:
-        qoldanushyEngizgenSan = input('Санды енгізіңіз: > ')
+        qoldanushyEngizgenSan = input('3 цифрдан тұратын санды енгізіңіз: > ')
         if jasyrynSan == qoldanushyEngizgenSan:
             print('Сіз жасырын санды таптыңыз.')
+        elif len(qoldanushyEngizgenSan) < 3 or len(qoldanushyEngizgenSan) >= 4:
+            continue
         else:
             print('Тағы байқап көріңіз')
             print(MUMKINDIKTER_SANY)
@@ -20,6 +22,21 @@ def main():
                 print(f'Жасырын сан: {jasyrynSan} болатын')
                 jasyrynSan = jasyrynSandyAlu()
                 MUMKINDIKTER_SANY = 1
+
+def tuspaldapKomekBeru(qoldanushyEngizgenSan, jasyrynSan):
+    if qoldanushyEngizgenSan == jasyrynSan:
+        print('Жарайсыз. Сіз жасырын санды таптыңыз!')
+    
+    komekHabarlama = []
+    for i in (len(qoldanushyEngizgenSan)):
+        if qoldanushyEngizgenSan[i] == jasyrynSan[i]:
+            komekHabarlama.append('Fermi')
+        elif qoldanushyEngizgenSan[i] in jasyrynSan:
+            komekHabarlama.append('Pico')
+        elif qoldanushyEngizgenSan[i] not in jasyrynSan:
+            komekHabarlama.append('Bagels')
+    ' '.join(komekHabarlama)
+    return komekHabarlama
 
 def jasyrynSandyAlu():
     jalpySandarTizimi = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
