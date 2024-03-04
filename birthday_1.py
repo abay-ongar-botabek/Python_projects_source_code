07367a433e34d39dc1f713d275e38ed17c4313f0
abay.ongar <abay.ongar.botabek@gmail.com>
2024-03-04 11:12:55 +0500

    tuylgan kun paradox kz fin1

--- birthday_1.py
+++ birthday_1.py
@@ -23,13 +23,51 @@ kezdoisoqTuylganKunderSany = int(jauap)
 # for i in range(len(tyuylganKunder)):
 #     print(tyuylganKunder[i])
 
-JUL_AILARY = ['Қаңтар', 'Ақпан', 'Наурыз', 'Сәуір', 'Мамыр', 'Маусым', 'Тамыз', 'Шілде', 'Қыркүйек', 'Қазан', 'Қараша', 'Желтоқсан']
+JUL_AILARY = ['Қаңтар', 'Ақпан', 'Наурыз', 'Сәуір', 'Мамыр', 'Маусым', 'Шілде', 'Тамыз', 'Қыркүйек', 'Қазан', 'Қараша', 'Желтоқсан']
 
-print(f'Міне {jauap} мысал туылған күндер.')
+print(f'Міне {jauap} туылған күн мысалы.')
 tuylganKunder = tuylganKunderAlu(kezdoisoqTuylganKunderSany)
 for i, tuylganKun in enumerate(tuylganKunder):
     if i != 0:
         print(', ', end='')
     jylAilary = JUL_AILARY[tuylganKun.month - 1]
-    kundiMatinmenJazu = f'{tuylganKun.day}-ші {jylAilary}'
+    kundiMatinmenJazu = f'{tuylganKun.day} {jylAilary}'
     print(kundiMatinmenJazu, end='')
+
+
+def saikesTk(tuylganKunder):
+    if len(tuylganKunder) == len(set(tuylganKunder)):
+        return None
+    
+    for a, tuylganKunA in enumerate(tuylganKunder):
+        for b, tuylganKunB in enumerate(tuylganKunder[a+1:]):
+            if tuylganKunA == tuylganKunB:
+                return tuylganKunA
+            
+saikesTuylganKunder = saikesTk(tuylganKunder)
+print()
+
+print('')
+if saikesTuylganKunder != None:
+    jylAilary = JUL_AILARY[tuylganKun.month - 1]
+    kundiMatinmenJazu = f'{tuylganKun.day} {jylAilary}'
+    print(f'{kundiMatinmenJazu} күні туылған бірнеше адам бар')
+else:
+    print('Бір күнде туылған адамдар табылмады')
+
+simSaikestigi = 0
+
+for i in range(100_000):
+    if i % 10_000 == 0:
+        i += 10000
+        print(f'{i} симуляция жүрді ...')
+    tuylganKunder = tuylganKunderAlu(tkSany)
+    if saikesTk(tuylganKunder) != None:
+        simSaikestigi += 1
+print('100 000 симуляция жүрді.')
+
+yqtyimaldyq = round(simSaikestigi/100_000 * 100, 2)
+print(f'{tkSany} адам үшін бірдей туылған күн ықтималдылығы: {yqtyimaldyq}')
+
+
+
